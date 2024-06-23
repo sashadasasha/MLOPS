b5edb3f795b16ac8a3efc410ee1f4055d535dd12
Sasha <sashadasasha@gmail.com>
2024-06-23 23:04:57 +0300

    fix

--- lab2/pipelines/jenkins.sh
+++ lab2/pipelines/jenkins.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-# Получение пути к директории Jenkins Home
-JENKINS_HOME="$JENKINS_HOME"
-
-# Путь к каталогу сборки
-BUILD_DIR="../"
-
-# Создание каталога build в корневом каталоге
-mkdir -p "$BUILD_DIR" || { echo "Ошибка при создании каталога 'build'."; exit 1; }
-
-
-# Создание виртуального окружения
-python3 -m venv "$BUILD_DIR/venv" || { echo "Ошибка при создании виртуального окружения."; exit 1; }
-
-# Активация виртуального окружения
-source "$BUILD_DIR/venv/bin/activate" || { echo "Ошибка при активации виртуального окружения."; exit 1; }
-
-# Установка зависимостей
-pip install -r "$BUILD_DIR/requirements.txt" || { echo "Ошибка при установке зависимостей."; exit 1; }
-
 echo "Запуск создания данных"
 python3 ../data_creation.py || { echo "Ошибка при выполнении data_creation.py"; exit 1; }
 
