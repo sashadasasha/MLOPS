fabd369e081fdb5aa56ee35e96dd50af28ec7ad7
Sasha <sashadasasha@gmail.com>
2024-06-23 23:05:57 +0300

    fix directory

--- lab2/pipelines/jenkins.sh
+++ lab2/pipelines/jenkins.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "Запуск создания данных"
-python3 ../data_creation.py || { echo "Ошибка при выполнении data_creation.py"; exit 1; }
+python3 ./data_creation.py || { echo "Ошибка при выполнении data_creation.py"; exit 1; }
 
 echo "Запуск предобработки данных"
-python3 ../model_preprocessing.py || { echo "Ошибка при выполнении model_preprocessing.py"; exit 1; }
+python3 ./model_preprocessing.py || { echo "Ошибка при выполнении model_preprocessing.py"; exit 1; }
 
 echo "Запуск подготовки и обучения модели"
-python3 ../model_preparation.py || { echo "Ошибка при выполнении model_preparation.py"; exit 1; }
+python3 ./model_preparation.py || { echo "Ошибка при выполнении model_preparation.py"; exit 1; }
 
 echo "Запуск тестирования модели"
-python3 ../model_testing.py || { echo "Ошибка при выполнении model_testing.py"; exit 1; }
+python3 ./model_testing.py || { echo "Ошибка при выполнении model_testing.py"; exit 1; }
